3034d1e8d3421886640c98925ac17a8cb7f102a5
delvedor <tommydelved@gmail.com>
2021-03-01 08:06:25 +0100

    Updated test

--- test.js
+++ test.js
@@ -26,73 +26,75 @@ test('Should render the entire page', async t => {
   t.true(response.payload.includes('foobar'))
 })
 
-test('Should return a turbo fragment', async t => {
-  const fastify = Fastify()
-  fastify.register(Hotwire, {
-    templates: join(__dirname, 'example', 'views'),
-    filename: join(__dirname, 'example', 'worker.js')
-  })
+function runTurboStream (action) {
+  test(`Should return a turbo fragment (${action})`, async t => {
+    const fastify = Fastify()
+    fastify.register(Hotwire, {
+      templates: join(__dirname, 'example', 'views'),
+      filename: join(__dirname, 'example', 'worker.js')
+    })
 
-  fastify.get('/', async (req, reply) => {
-    return reply.turboStream.replace(
-      'message.svelte',
-      'messages',
-      {
-        message: {
-          id: 'unique',
-          text: 'hello world',
-          user: 'foobar'
+    fastify.get('/', async (req, reply) => {
+      return reply.turboStream[action](
+        'message.svelte',
+        'messages',
+        {
+          message: {
+            id: 'unique',
+            text: 'hello world',
+            user: 'foobar'
+          }
         }
-      }
-    )
+      )
+    })
+
+    const response = await fastify.inject({
+      method: 'GET',
+      path: '/'
+    })
+
+    t.strictEqual(response.statusCode, 200)
+    t.match(response.headers, { 'content-type': 'text/vnd.turbo-stream.html; charset=utf-8' })
+    t.strictEqual(response.payload.replace(/\n/g, '').trim(), `<turbo-stream action="${action}" target="messages">    <template>      <turbo-frame id="message_frame_unique"><p><strong>foobar:</strong> hello world</p>  <form action="/message/unique/delete" method="POST"><button type="submit">Remove</button></form></turbo-frame>    </template>  </turbo-stream>`)
   })
+}
 
-  const response = await fastify.inject({
-    method: 'GET',
-    path: '/'
-  })
+function runTurboGenerate (action) {
+  test(`Should generate a turbo fragment (${action})`, async t => {
+    const fastify = Fastify()
+    fastify.register(Hotwire, {
+      templates: join(__dirname, 'example', 'views'),
+      filename: join(__dirname, 'example', 'worker.js')
+    })
 
-  t.strictEqual(response.statusCode, 200)
-  t.match(response.headers, { 'content-type': 'text/vnd.turbo-stream.html; charset=utf-8' })
-  t.strictEqual(response.payload, `
-  <turbo-stream action="replace" target="messages">
-    <template>
-      <turbo-frame id="message_frame_unique"><p><strong>foobar:</strong> hello world</p>
-
-  <form action="/message/unique/delete" method="POST"><button type="submit">Remove</button></form></turbo-frame>
-    </template>
-  </turbo-stream>
-`)
-})
-
-test('Should generate a turbo fragment', async t => {
-  const fastify = Fastify()
-  fastify.register(Hotwire, {
-    templates: join(__dirname, 'example', 'views'),
-    filename: join(__dirname, 'example', 'worker.js')
-  })
-
-  fastify.get('/', async (req, reply) => {
-    reply.type('text/plain')
-    return reply.turboGenerate.replace(
-      'message.svelte',
-      'messages',
-      {
-        message: {
-          id: 'unique',
-          text: 'hello world',
-          user: 'foobar'
+    fastify.get('/', async (req, reply) => {
+      reply.type('text/plain')
+      return reply.turboGenerate[action](
+        'message.svelte',
+        'messages',
+        {
+          message: {
+            id: 'unique',
+            text: 'hello world',
+            user: 'foobar'
+          }
         }
-      }
-    )
-  })
+      )
+    })
 
-  const response = await fastify.inject({
-    method: 'GET',
-    path: '/'
-  })
+    const response = await fastify.inject({
+      method: 'GET',
+      path: '/'
+    })
 
-  t.strictEqual(response.statusCode, 200)
-  t.match(response.headers, { 'content-type': 'text/plain' })
-  t.strictEqual(response.payload, '<turbo-stream action="replace" target="messages">    <template>      <turbo-frame id="message_frame_unique"><p><strong>foobar:</strong> hello world</p>  <form action="/message/unique/delete" method="POST"><button type="submit">Remove</button></form></turbo-frame>    </template>  </turbo-stream>')
-})
+    t.strictEqual(response.statusCode, 200)
+    t.match(response.headers, { 'content-type': 'text/plain' })
+    t.strictEqual(response.payload, `<turbo-stream action="${action}" target="messages">    <template>      <turbo-frame id="message_frame_unique"><p><strong>foobar:</strong> hello world</p>  <form action="/message/unique/delete" method="POST"><button type="submit">Remove</button></form></turbo-frame>    </template>  </turbo-stream>`)
+  })
+}
+
+const actions = ['append', 'prepend', 'replace', 'update', 'remove']
+for (const action of actions) {
+  runTurboStream(action)
+  runTurboGenerate(action)
+}
